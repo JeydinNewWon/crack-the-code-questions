8e6cfb342a2904d21a00baa798138ae4d35103ca
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-06-17 18:16:25 +1000

    added java sol

---
+++ Easy/LastStoneWeight/Solution.java
@@ -0,0 +1,23 @@
+package Easy.LastStoneWeight;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        for (int stone : stones) maxHeap.add(-stone);
+
+        while (maxHeap.size() > 1) {
+            int stone1 = -1 * maxHeap.poll();
+            int stone2 = -1 * maxHeap.poll();
+
+            if (stone1 != stone2) {
+                maxHeap.add(-1 * (Math.max(stone1, stone2) - Math.min(stone1, stone2)));
+            }
+        } 
+
+        if (maxHeap.isEmpty()) return 0;
+
+        return -1 * maxHeap.poll();
+    }
+}
